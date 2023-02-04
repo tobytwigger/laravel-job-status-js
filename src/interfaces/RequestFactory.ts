bde2e8876cf799b08902ec5914f8bceddaed0e8c
Toby Twigger <toby.twigger@swytchbike.com>
2023-02-04 23:04:56 +0000

    Updates

--- src/interfaces/RequestFactory.ts
+++ src/interfaces/RequestFactory.ts
@@ -2,13 +2,14 @@ import Request from "~/client/Request";
 import {JobRun} from "~/interfaces/models/JobRun";
 import {resolveHandler} from "~/handler/HandlerManager";
 import handle from "~/client/ClientFactory";
+import Notifier from "~/handler/Notifier";
 
 export default abstract class RequestFactory<ReturnType> {
 
     abstract create(): Request
 
-    public onUpdate(handler: (jobs: ReturnType) => void): string {
-        return resolveHandler(this).handle(this.create(), handler);
+    public listen(): Notifier<ReturnType> {
+        return new Notifier<ReturnType>();
     }
 
     public get(): Promise<ReturnType> {
--- tests/requests/jobs/list.spec.ts
+++ tests/requests/jobs/list.spec.ts
@@ -5,6 +5,8 @@ import Request from "../../../src/client/Request";
 it('request object can be validly created', () => {
     let requestObject: Request = client.jobs.index().create();
 
+    client.jobs.index().listen()
+        .on;
     expect(requestObject).toBeInstanceOf(Request);
     expect(requestObject.method).toBe("GET");
     expect(requestObject.url).toBe("/jobs");
