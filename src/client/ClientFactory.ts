9de8d1f6e66476519e37b2973f3c0ad8065bd7f6
Toby Twigger <tobytwigger1@gmail.com>
2023-02-05 18:05:30 +0000

    Pass data in post requests

--- src/client/ClientFactory.ts
+++ src/client/ClientFactory.ts
@@ -1,16 +1,24 @@
 import Request from "~/client/Request";
-import axios, {AxiosPromise} from "axios";
+import axios, {AxiosPromise, AxiosRequestConfig} from "axios";
 import {baseUrl} from "~/config/config";
 
 export default function handle(request: Request): AxiosPromise<unknown> {
-    return axios.request({
+    let config: AxiosRequestConfig = {
         url: request.url,
         method: request.method,
-        baseURL: baseUrl()
+        baseURL: baseUrl(),
         // baseURL?: string;
         // headers?: AxiosRequestHeaders;
         // params?: any;
         // data?: D;
         // withCredentials?: boolean;
-    });
+
+    }
+    let data = request.data;
+    if(Object.keys(data).length > 0) {
+        config.data = data;
+    }
+    return axios.request(config);
+
+
 }
